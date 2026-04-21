2d73b80e3a7ed79e78ac612bcce58a2bc526f26e
spcameron <cameron.seanpatrick@gmail.com>
2026-04-21 10:57:06 -0400

    add scribe.go

---
+++ scribe.go
@@ -0,0 +1,48 @@
+package scribe
+
+import (
+	"io"
+
+	"golang.org/x/net/html"
+)
+
+type Document interface {
+	Write(io.Writer) error
+}
+
+// Compile parses Markdown and returns a renderable document.
+func Compile(md string) (Document, error) {
+	src := source.NewSource(md)
+
+	irDoc, err := block.Parse(src)
+	if err != nil {
+		return nil, err
+	}
+
+	astDoc, err := lower.Document(irDoc)
+	if err != nil {
+		return nil, err
+	}
+
+	tree, err := codegen.HTML(astDoc)
+	if err != nil {
+		return nil, err
+	}
+
+	return tree, nil
+}
+
+// HTML parses Markdown and renders the result as an HTML string.
+func HTML(md string) (string, error) {
+	tree, err := Compile(md)
+	if err != nil {
+		return "", err
+	}
+
+	htmlStr, err := html.Render(tree)
+	if err != nil {
+		return "", err
+	}
+
+	return htmlStr, nil
+}
